09f340fdf77760d3945ebacf1477d9755ac4c057
ahndy84 <ahndy84@gmail.com>
2020-01-11 17:26:14 +0900

    Initial commit

---
+++ src/main/java/com/salt/smayouth/SmayouthApplication.java
@@ -0,0 +1,13 @@
+package com.salt.smayouth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmayouthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmayouthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salt/smayouth/SmayouthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salt.smayouth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmayouthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
